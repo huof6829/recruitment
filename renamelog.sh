1fb17fe2f6a1ad0b218fae9937577c8cf3d57598
huof6890 <68298506@qq.com>
2022-05-18 09:52:01 +0800

    rename in commited git log

---
+++ renamelog.sh
@@ -0,0 +1,17 @@
+## 修改已经提交的姓名邮箱
+git filter-branch --env-filter '
+WRONG_EMAIL="old@xxx.com"
+NEW_NAME="huf"
+NEW_EMAIL="6829@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
